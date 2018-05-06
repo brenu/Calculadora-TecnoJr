b2b1f0884e3627575b5e1c18a659c45c887fcebd
Breno Vitório <bvsousa64@gmail.com>
2018-05-06 00:11:56 -0300

    Esse js não funciona :(

---
+++ includes/head.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<link rel="stylesheet" type="text/css" media="all" href="css/materialize.css">
+<link rel="stylesheet" type="text/css" media="all" href="css/calculadora.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+
+<title>Calculadora V1</title>
---
+++ js/calculadora.js
@@ -0,0 +1,6 @@
+function noClique(){
+	/*Imprime na div "lcd" o texto*/
+	document.getElementById("lcd").innerHTML = "teste";
+	alert("teste");
+
+}
